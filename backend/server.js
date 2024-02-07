73f95b0fa26a8abdeb078a3df58c0e1361e589f3
SHIVAM KASERA <skshivamkasera2003@gmail.com>
2024-02-07 09:41:29 +0530

    Update Code

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv'
 import { database_connection } from './config/database_connection.js'
 import { connectPassport } from './utils.js'
 import Razorpay from 'razorpay'
+import path from "path"
+import express from 'express'
+import { fileURLToPath } from 'url'
 
 dotenv.config({ path: "./backend/config/config.env" })
 
@@ -12,6 +15,14 @@ connectPassport()
 export const instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET
+})
+
+const fileName = fileURLToPath(import.meta.url)
+const dirName = path.dirname(fileName)
+
+app.use(express.static(path.join(dirName, "../client/build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirName, "../client/build/index.html"))
 })
 
 app.listen(process.env.PORT, () => {
